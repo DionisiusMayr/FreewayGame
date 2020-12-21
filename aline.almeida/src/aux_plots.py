02888e7e0b7d8ec0dfea2098e3370b96804f65c2
Aline Almeida <aline.gabriel.almeida@gmail.com>
2020-12-21 19:34:35 -0300

    QLearning plots update

--- aline.almeida/src/aux_plots.py
+++ aline.almeida/src/aux_plots.py
@@ -37,7 +37,7 @@ def plot_rewards(total_rewards):
     plt.ylabel("Final Reward")
     
     
-def plot_3scores(s1, s2, s3):
+def plot_3scores(s1, s2, s3, lab1, lab2, lab3):
     fig = plt.figure(figsize=FIGSIZE, dpi=DPI)
     ax = fig.add_subplot(1, 1, 1)
     l = len(s1)
@@ -47,9 +47,9 @@ def plot_3scores(s1, s2, s3):
 
     ax.set_xlim(0.9, l + 0.1)
     ax.set_ylim(min(s2)-2, max(s2)+2)
-    ax.plot(range(1, l + 1), s1, color='cyan', label="gamma 0.99", alpha=0.8)
-    ax.plot(range(1, l + 1), s2, color='magenta', label="gamma 0.90", alpha=0.5)
-    ax.plot(range(1, l + 1), s3, color='red', label="Mean baseline")
+    ax.plot(range(1, l + 1), s1, color='cyan', label=lab1, alpha=0.8)
+    ax.plot(range(1, l + 1), s2, color='magenta', label=lab2, alpha=0.5)
+    ax.plot(range(1, l + 1), s3, color='red', label=lab3)
     plt.legend()
     
     plt.xlabel("Episode")
