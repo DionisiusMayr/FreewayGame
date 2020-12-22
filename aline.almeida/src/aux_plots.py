3ebf7f062bbc3b21f80db975f72ac476171190f7
Aline Almeida <aline.gabriel.almeida@gmail.com>
2020-12-22 16:34:39 -0300

    QL and MC

--- aline.almeida/src/aux_plots.py
+++ aline.almeida/src/aux_plots.py
@@ -40,17 +40,23 @@ def plot_rewards(total_rewards):
 def plot_3scores(s1, s2, s3, lab1, lab2, lab3):
     fig = plt.figure(figsize=FIGSIZE, dpi=DPI)
     ax = fig.add_subplot(1, 1, 1)
-    l = len(s1)
-
-    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
-
-    ax.set_xlim(0.9, l + 0.1)
-    ax.set_ylim(min(s2)-2, max(s2)+2)
-    ax.plot(range(1, l + 1), s1, color='cyan', label=lab1, alpha=0.8)
-    ax.plot(range(1, l + 1), s2, color='magenta', label=lab2, alpha=0.5)
-    ax.plot(range(1, l + 1), s3, color='red', label=lab3)
+    
+    ax.plot(s1, color='cyan', label=lab1, alpha=0.8)
+    ax.plot(s2, color='magenta', label=lab2, alpha=0.5)
+    ax.plot(s3, color='red', label=lab3)
     plt.legend()
     
     plt.xlabel("Episode")
     plt.ylabel("Final Score")
+    
+def plot_3rewards(r1, r2, r3, lab1, lab2, lab3):
+    fig = plt.figure(figsize=FIGSIZE, dpi=DPI)
+    ax = fig.add_subplot(1, 1, 1)
+ 
+    ax.plot(r1, color='cyan', label=lab1, alpha=0.8)
+    ax.plot(r2, color='magenta', label=lab2, alpha=0.5)
+    ax.plot(r3, color='red', label=lab3)
+    plt.legend()
+    
+    plt.xlabel("Episode")
+    plt.ylabel("Final Reward")
