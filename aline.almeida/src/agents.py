461ab83f6d36cf528a9e1f3db80cd589f666feea
Aline Almeida <aline.gabriel.almeida@gmail.com>
2020-12-24 05:07:07 -0300

    Include model serializer

--- aline.almeida/src/agents.py
+++ aline.almeida/src/agents.py
@@ -27,8 +27,7 @@ class Baseline(Agent):
     def __init__(self):
         pass
     
-    ##- def act(self, state):
-    def act(self, state, reward):
+    def act(self, state):
         return 1  # Always move up!
 
 
@@ -36,43 +35,47 @@ class MonteCarloControl(Agent):
     def __init__(self, gamma: float, available_actions: int, N0: float):
         self.gamma = gamma
         self.available_actions = available_actions
-        
-
-        self.Q = defaultdict(lambda: np.zeros(self.available_actions))
-        # TODO: Are we able to use numpy arrays for `Returns`?
-        self.Returns = defaultdict(lambda: defaultdict(list))
-        self.pi = defaultdict(lambda: 1)  # Forward Bias
         self.N0 = N0
 
+        self.Q = defaultdict(lambda: np.zeros(self.available_actions))
+        self.Nsa = defaultdict(lambda: defaultdict(lambda: 0))
+        self.state_visits = defaultdict(lambda: 0)
+        self.pi = defaultdict(lambda: 1)  # Forward Bias
+
     def act(self, state):
-        visits_on_state = sum([len(v) for k, v in self.Returns[state].items()])
-        epsilon = self.N0 / (self.N0 + visits_on_state)
-                     
-        if np.random.choice(np.arange(2), size=1, p=np.array([1-epsilon, epsilon])):
-            return np.random.choice(self.available_actions)  # Explore!
+        epsilon = self.N0 / (self.N0 + self.state_visits[state])
+ 
+        if np.random.choice(np.arange(self.available_actions), p=[1 - epsilon, epsilon]):
+            action = np.random.choice(self.available_actions)  # Explore!
         else:
-            return self.pi[state]  # Greedy
+            action = self.pi[state]  # Greedy
+
+        return action
 
     def update_policy(self, episode):
         G = 0
-        S = np.array([s for s, _, _, _ in episode])
-        A = np.array([a for _, a, _, _ in episode])
-        R = np.array([r for _, _, r, _ in episode])
+        S = np.array(episode.S)
+        A = np.array(episode.A)
+        R = np.array(episode.R)
 
         for t in reversed(range(episode.length - 1)):
-            # TODO: add the action to this comment
-            # TODO: According to the algorithm I should check if S_t appers in
-            #  the sequence S_0, S_1, S_2, ..., S_t-1.
+            self.state_visits[S[t]] += 1
+            self.Nsa[S[t]][A[t]] += 1
+
+            alpha = (1 / self.Nsa[S[t]][A[t]])
             G = self.gamma * G + R[t + 1]
-            self.Returns[S[t]][A[t]].append(G)
-            # Alpha is the `len(self.Returns[S[t]][A[t]])`
-            self.Q[S[t]][A[t]] = sum(self.Returns[S[t]][A[t]]) / len(self.Returns[S[t]][A[t]])  # Mean
+
+            self.Q[S[t]][A[t]] += alpha * (G - self.Q[S[t]][A[t]])
             self.pi[S[t]] = self.Q[S[t]].argmax()
 
-#         print(f"Pi: {len(pi):8} ", end='')#, Q: {len(Q)}, Returns: {len(Returns)}")
+#         print(f"Pi: {len(pi):8} ", end='')
 
         return episode.get_final_score(), episode.get_total_reward()
 
+    def print_parameters(self):
+        print(f"gamma = {self.gamma}")
+        print(f"available_actions = {self.available_actions}")
+        print(f"N0 = {self.N0}")
 
 class QLearning(Agent):
     def __init__(self, gamma: float, available_actions: int, N0: float):
@@ -87,31 +90,69 @@ class QLearning(Agent):
     def act(self, state):
         epsilon = self.N0 / (self.N0 + self.state_visits[state])
 
-        if np.random.choice(np.arange(2), size=1, p=np.array([1-epsilon, epsilon])):
+        if np.random.choice(np.arange(self.available_actions), p=[1 - epsilon, epsilon]):
             action = np.random.choice(self.available_actions)  # Explore!
-#         elif self.state_visits[state] == 0:
         elif self.Q[state].max() == 0.0 and self.Q[state].min() == 0.0:
             action = 1  # Bias toward going forward
         else:
             action = self.Q[state].argmax()  # Greedy action
 
         self.state_visits[state] += 1
-
         self.Nsa[state][action] += 1
 
         return action
 
     def update_Q(self, old_state, new_state, action, reward):
-#         if reward:
-#             print("Old Q[state][action]", self.Q[old_state][action])
-#             print(f"alpha {self.alpha}, reward {reward}, gamma {self.gamma}, right side {(reward + (self.gamma * self.Q[new_state].max()) - self.Q[old_state][action])}")
         alpha = (1 / self.Nsa[old_state][action])
-        self.Q[old_state][action] = self.Q[old_state][action] + alpha * (reward + (self.gamma * self.Q[new_state].max()) - self.Q[old_state][action])
-#         if reward:
-#             print("New Q[state][action]", self.Q[old_state][action])
+        self.Q[old_state][action] += alpha * (reward + (self.gamma * self.Q[new_state].max()) - self.Q[old_state][action])
 
+    def print_parameters(self):
+        print(f"gamma = {self.gamma}")
+        print(f"available_actions = {self.available_actions}")
+        print(f"N0 = {self.N0}")
 
-if __name__ == '__main__':
-    print('Testing agents.py...')
-    agent = Baseline()
-    print('All good!')
+class SarsaLambda(Agent):
+    def __init__(self, gamma: float, available_actions: int, N0: float, lambd: float):
+        self.gamma = gamma
+        self.available_actions = available_actions
+        self.N0 = N0
+        self.lambd = lambd
+
+        self.Q = defaultdict(lambda: np.zeros(self.available_actions))
+        self.state_visits = defaultdict(lambda: 0)
+        self.Nsa = defaultdict(lambda: defaultdict(lambda: 0))
+        self.E = defaultdict(lambda: np.zeros(self.available_actions))
+
+    def act(self, state):
+        epsilon = self.N0 / (self.N0 + self.state_visits[state])
+
+        if np.random.choice(np.arange(self.available_actions), p=[1 - epsilon, epsilon]):
+            action = np.random.choice(self.available_actions)  # Explore!
+        elif self.Q[state].max() == 0.0 and self.Q[state].min() == 0.0:
+            action = 1  # Bias toward going forward
+        else:
+            action = self.Q[state].argmax()  # Greedy action
+
+        self.state_visits[state] += 1
+        self.Nsa[state][action] += 1
+        self.E[state][action] += 1
+
+        return action
+
+    def update_Q(self, old_s, new_s, old_a, new_a, reward):
+        delta = reward + self.gamma * self.Q[new_s][new_a] - self.Q[old_s][old_a]
+        alpha = (1 / self.Nsa[old_s][old_a])
+
+        for s in self.E:
+            for a in range(self.available_actions):
+                self.Q[old_s][old_a] += alpha * delta * self.E[s][a]
+                self.E[s][a] = self.gamma * self.lambd * self.E[s][a]
+
+    def reset_E(self):
+        self.E = defaultdict(lambda: np.zeros(self.available_actions))
+
+    def print_parameters(self):
+        print(f"gamma = {self.gamma}")
+        print(f"available_actions = {self.available_actions}")
+        print(f"N0 = {self.N0}")
+        print(f"lambd = {self.lambd}")
